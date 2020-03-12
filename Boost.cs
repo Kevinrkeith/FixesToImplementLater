9336ef10764286494b15a07e2c144f48e204a254
Kevinrkeith <46653751+Kevinrkeith@users.noreply.github.com>
2020-03-12 13:46:07 -0400

    Add files via upload

---
+++ Boost.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Boost : PowerUp
+{
+    [SerializeField]
+    float multiplier;
+    public override void UsePowerUp(int playerUsed)
+    {
+        PlayerBody pb = GameManager.gameManager.playerBody[playerUsed];
+        pb.GetComponent<Rigidbody>().AddForce(multiplier*GameManager.gameManager.cameras[playerUsed].transform.forward, ForceMode.VelocityChange);
+    }
+}
